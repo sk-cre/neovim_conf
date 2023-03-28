ab33d7f587ebe171cce5e62ad01a41b0f5b01d75
sk-cre <dinice.sk@gmail.com>
2023-03-28 18:39:45 +0900

    second

--- lua/compete.lua
+++ lua/compete.lua
@@ -1,13 +1,17 @@
+--local home_dir = vim.fn.expand("~/Documents/Compete")
+--local snip_dir = home .. "/snippet"
+--local playground_dir = home .. "/play_ground"
+--local atcoder_dir = home .. "/atcoder"
+
 local home = vim.fn.expand('~/Documents/codefolder/atcoder')
 local snip = vim.fn.expand("~/Documents/codefolder/atcoder/snippet")
 local playground = vim.fn.expand("~/Documents/codefolder/play_ground")
 --local vscode_snip = vim.fn.expand("~/AppData/Roaming/Code/User/snippets/rust.json")
-vim.g.coc_snippet_root = vim.fn.expand(snip.."/coc-snippets")
-local aturl = "https://atcoder.jp/contests/"
+local atcoder_url = "https://atcoder.jp/contests/"
 local osname = vim.loop.os_uname().sysname
-local open_url = (osname == "Windows_NT" or osname == "Darwin") and "!start " or (osname == "Linux" and "!exploler.exe " or nil)
+local open_url = (osname == "Windows_NT" and "!start ") or (osname == "!Darwin" and "!open ") or (osname == "Linux" and "!exploler.exe ") or nil
 
-function pg()
+function open_playground()
     vim.cmd("tabnew Playground")
     vim.cmd("lcd " .. playground)
     vim.cmd("e ./src/main.rs | %d | r ./src/template.rs | w")
@@ -61,24 +65,27 @@ end
 
 function watch_term(contest,problem)
     local problem = problem or "a"
-    vim.cmd("tabnew ".. contest .. " " .. problem)
-    --if vim.fn.winnr('$') > 1 then
-        --vim.cmd("only")
-    --end
+    vim.cmd("tabnew")
     local full = vim.fn.expand(home .. "/" .. contest)
     if vim.fn.isdirectory(full) == 0 then
         vim.cmd("lcd " .. home)
-        vim.cmd("!cargo compete new " .. contest)
+        local res = vim.fn.system("cargo compete new " .. contest)
+        local exit_code = vim.v.shell_error
+        if exit_code ~= 0 then
+            print("Invalid contest name")
+            vim.cmd("tabc")
+            return 0
+        end
         vim.cmd("lcd -")
     end
-    vim.cmd("lcd " .. full)
-    vim.cmd("e ./src/bin/" .. problem .. ".rs")
-    vim.cmd(open_url .. get_problem_url(problem))
+    vim.cmd("silent lcd " .. full)
+    vim.cmd("silent e ./src/bin/" .. problem .. ".rs")
+    vim.cmd("silent " .. open_url .. get_problem_url(problem))
     vim.cmd("40vs")
-    vim.cmd(string.format("terminal cargo watch -x \"compete t %s\"", problem)) 
-    vim.cmd("norm G")
-    vim.cmd("wincmd l")
-    vim.cmd("5 | stopinsert")
+    vim.cmd("silent " .. string.format("terminal cargo watch -x \"compete t %s\"", problem)) 
+    vim.cmd("silent norm G")
+    vim.cmd("silent wincmd l")
+    vim.cmd("silent 5 | silent stopinsert")
 end
 
 local bg_color = "Olive" -- 任意の背景色を設定
@@ -99,15 +106,15 @@ function floating_term(command)
     vim.cmd(command)
 end
 
-vim.api.nvim_create_user_command("Playground",function()pg()end,{})
+vim.api.nvim_create_user_command("Playground",function()open_playground()end,{})
 vim.api.nvim_create_user_command("Snippet",function()open_snippet()end,{})
 vim.api.nvim_create_user_command("SnippetWrite",function()write_snippet()end,{})
 vim.api.nvim_create_user_command("NN",function(opts)watch_term(vim.split(opts.args," ")[1],vim.split(opts.args," ")[2])end,{nargs = "+"})
 vim.api.nvim_create_user_command("Np",function(opts)watch_term(get_contest(),opts.args)end,{nargs = 1})
 vim.api.nvim_create_user_command("Watch",function()floating_term(":term cargo compete w submissions atcoder " .. get_contest())end , {} )
 
-vim.api.nvim_create_user_command("Me", function()vim.cmd(open_url .. aturl .. get_contest() .. '/submissions/me')end,{})
-vim.api.nvim_create_user_command("Rank", function()vim.cmd(open_url .. aturl .. get_contest() .. '/standings')end,{})
+vim.api.nvim_create_user_command("Me", function()vim.cmd(open_url .. atcoder_url .. get_contest() .. '/submissions/me')end,{})
+vim.api.nvim_create_user_command("Rank", function()vim.cmd(open_url .. atcoder_url .. get_contest() .. '/standings')end,{})
 vim.api.nvim_create_user_command("Open", function()vim.cmd(open_url .. get_problem_url() )end,{})
 vim.api.nvim_create_user_command("Test", function()floating_term('term cargo compete t ' .. get_problem() )end,{})
 vim.api.nvim_create_user_command("TestR", function()floating_term('term cargo compete t ' .. get_problem() .." --release")end,{})
